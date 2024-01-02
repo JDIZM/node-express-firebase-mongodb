8af61578ba47f983830cb3c4a05a0422877e1a31
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-01-02 08:08:07 +0000

    chore: add config file for env vars

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+const ENV = process.env.NODE_ENV ?? "development";
+
+console.log("mode:", ENV);
+
+if (ENV !== "production") {
+  dotenv.config();
+}
+
+export const config = {
+  env: ENV,
+  port: process.env.PORT || 3000
+};
