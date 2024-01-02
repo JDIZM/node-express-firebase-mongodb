90054570319280630430cec67fb6c6f21835ccde
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-01-02 07:38:35 +0000

    feat: connect prisma and add a simple handler to get users from db

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,15 @@
 import { Application } from "express";
 import { test } from "@/helpers/index.js";
+import { getUsers } from "@/handlers/users/users.handlers.js";
 
 export const API_ROUTES = {
   root: "/",
-  example: "/example"
+  users: "/users"
 };
 
 export function routes(app: Application) {
   app.get(API_ROUTES.root, (req, res) => {
     res.send(`Routes are active! route: ${API_ROUTES.root} with test ${test}`);
   });
-  app.get(API_ROUTES.example, (req, res) => {
-    res.send(`Routes are active! route: ${API_ROUTES.example}`);
-  });
+  app.get(API_ROUTES.users, getUsers);
 }
