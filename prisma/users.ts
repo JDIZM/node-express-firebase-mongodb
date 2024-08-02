bf57e756021f054b45679f2fca32ced82eb01c0e
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-08-02 23:56:13 +0100

    feat: remove password and bcrypt

--- prisma/users.ts
+++ prisma/users.ts
@@ -1,18 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 export async function seedUsers(): Promise<void> {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash("example123", salt);
 
   const adminUser = await prisma.users.upsert({
     where: { email: "fred@flintstones.com" },
     update: {},
     create: {
       username: "fred",
-      password: hashedPassword,
       email: "fred@flintstones.com",
       claims: ["admin", "user"],
       sub: "123456789"
@@ -24,7 +20,6 @@ export async function seedUsers(): Promise<void> {
     update: {},
     create: {
       username: "barney",
-      password: hashedPassword,
       email: "barney@flintstones.com",
       claims: ["user"],
       sub: "987654321"
