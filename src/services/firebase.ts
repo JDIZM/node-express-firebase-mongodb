7c2b5046eeac705cf6eca469939086436076e290
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-08-02 23:56:40 +0100

    chore: firebase service config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase-admin/app";
+import { initializeApp, applicationDefault } from "firebase-admin/app";
 
 // see: https://firebase.google.com/docs/admin/setup for more info on setting up the firebase admin sdk
 export const app = initializeApp({
   // specify some options.
+  credential: applicationDefault()
 });
